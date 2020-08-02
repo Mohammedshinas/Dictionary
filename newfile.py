2513f9570fc13294ab232ecb61c060e1ba37e085
Mohammedshinas <69067639+Mohammedshinas@users.noreply.github.com>
2020-08-02 15:16:01 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,22 @@
+timetable={'Monday' :
+	                  {'h1' :
+	                  	{'Subject' : 'Ds' ,
+	                  	'Teacher' : 'Samad sir' ,
+	                  	'Room no' : '123'
+	                  	}
+	                  },
+	                  'Tuesday' :
+	                    {'h2' :
+	                    	{'Subject' : 'Cs' ,
+	                    	 'Teacher' : 'Haneesh sir' ,
+	                    	 'Room no' : '124'
+	                    	}
+	                    }
+                     }
+print(timetable['Monday']['h1']['Subject'])
+print(timetable['Tuesday']['h2']['Subject'])                                               
+	                    
+	                    
+	                    
+	                    
+	                  	
